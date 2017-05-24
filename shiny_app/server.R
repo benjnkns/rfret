04525be0dcc271893114ca5840d1205a76e53455
Ben Jenkins <benjnkns@gmail.com>
2017-05-24 10:45:34 -0600

    save plots with downlad button

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,106 +1,117 @@
-library(shiny)
+## SERVER.R
 library(ggplot2)
-library(magrittr)
-library(dplyr)
-require(reshape2)
+library(gridExtra)
+source("../R/inspect_raw_data.R")
 
-shinyServer(function(input, output,session) {
-
-    dataframe<-reactive({
-        if (is.null(input$datafile))
+function(input, output) {
+    #file_index = 1
+    values <- reactiveValues()
+    values$file_index <- 1
+    values$dataset_decisions <- list()
+    input_files <- reactive({
+        if (is.null(input$data_file)) {
+            # User has not uploaded a file yet
             return(NULL)
-        data<-read.csv(input$datafile$datapath, skip = 4)
-       # data<- data %>% group_by(C) %>% mutate(A) %>%
-        #    mutate(B) %>% mutate(add = (A+B)) %>% mutate(sub = (A-B))
-        #data
+        }
+        objectsLoaded <- list()
+        for(i in 1:length(input$data_file$name)){
+            df <- read.csv(input$data_file[i, 'datapath'],
+                           header=input$header,
+                           sep=input$sep,
+                           quote=input$quote,
+                           skip = input$skip_rows)
+            df <- list(input$data_file$name[i], df)
+            objectsLoaded[[length(objectsLoaded)+1]] <- df
+        }
+        return(objectsLoaded)
     })
-    output$table <- renderTable({
-        dataframe()
+
+    myData <- reactive({
+        df<-input_files()
+        if (is.null(df)) return(NULL)
+        return(df)
     })
-    output$plot <- renderPlot({
-        if(!is.null(dataframe()))
-            #ggplot(dataframe(),aes(x=X,y=add))+geom_point()
-           inspect_raw_data(dataframe())
+
+    output$filename <-renderText({myData()[[values$file_index]][[1]]})
+    output$raw_output <- renderPlot({
+        values$number_of_files <- length(myData())
+        if(is.null(input$data_file)) return(print("Please upload some FRET data"))
+        df <- data.frame(myData()[[values$file_index]][[2]])
+        raw_data_plots <- inspect_raw_data(df)
+        raw_data_grid <- grid.arrange(raw_data_plots$donor, raw_data_plots$acceptor, raw_data_plots$fret, ncol=2, nrow=3)
     })
+
     output$downloadPlot <- downloadHandler(
         filename = function() { paste(input$dataset, '.png', sep='') },
         content = function(file) {
-            print(dataframe())
-            fit <- inspect_raw_data(dataframe())
-            # pdf(filename="/Users/benjenkins/Documents/Hackathon/andrew-fork/rfret/plotImage.pdf")
-            #plot(fit)
-            png("/Users/benjenkins/Documents/Hackathon/andrew-fork/rfret/plotImage.png")
-            plot(1:10)
-            dev.off
-            # ggsave("/Users/benjenkins/Documents/Hackathon/andrew-fork/rfret/plotImage.png", plot = inspect_raw_data(dataframe()), device = "png")
-        }
-    )
-})
+            #png("/Users/benjenkins/Documents/Hackathon/andrew-fork/rfret/plotImage.png")
+            #plot(1:20)
+            #dev.off()
+            ggsave("/Users/benjenkins/Documents/Hackathon/andrew-fork/rfret/plotImage.png", plot = last_plot(), device = NULL, path = NULL, scale = 1, width = NA, height = NA, dpi = 300, limitsize = TRUE)
+    })
 
+    observeEvent(input$accept, label = "Accept", {
+        cat("accept ", values$file_index, "\n")
+        values$dataset_decisions[[length(values$dataset_decisions) + 1]] <- TRUE
+        values$file_index <- values$file_index + 1
+        View(values$dataset_decisions)
+    })
+    observeEvent(input$remove, label = "Remove", {
+        cat("remove ", values$file_index, "\n")
+        values$dataset_decisions[[length(values$dataset_decisions) + 1]] <- FALSE
+        values$file_index <- values$file_index + 1
+        View(values$dataset_decisions)
+    })
+    observeEvent(input$accept_all, label = "Accept All", {
+        cat("Accept All", values$file_index, "\n")
+        values$dataset_decisions[values$file_index:values$number_of_files] <- TRUE
+        values$file_index <- values$number_of_files
 
-#
-# library(shiny)
-# library(gridExtra)
-# library(ggplot2)
-# i = 1
-# shinyServer(function(input, output, session) {
-#     source("/Users/benjenkins/Documents/Hackathon/andrew-fork/rfret/R/inspect_raw_data.R")
-#     file_list <- c("")
-#     reactive_data <- renderText({
-#         print("Please printing")
-#        for(i in 1:length(input$input_files[,1]))
-#        {
-#            new_dataframe <- read.csv(input$input_files[i, 'datapath'],
-#                                  # header=input$header,
-#                                  sep=input$sep,
-#                                  quote=input$quote,
-#                                  skip = input$skip_rows)
-#            comment(new_dataframe) <- input$input_files$name[i]
-#            print(class(new_dataframe))
-#            if(input$header == FALSE){
-#                colnames(new_dataframe) <- c("Well Row","Well Col","Content",
-#                                          "fret_channel","donor_channel",
-#                                          "acceptor_channel","concentration")
-#            }
-#            file_list <- c(file_list, new_dataframe)
-#        }
-#        output$file_list <- file_list
-#    })
-#
-#     dataframe<-reactive({
-#         if (is.null(input$input_files))
-#             print ("got a null datafile")
-#             return(NULL)
-#         data<-read.csv(input$datafile$datapath)
-#         data<- data %>% group_by(C) %>% mutate(A) %>%
-#             mutate(B) %>% mutate(add = (A+B)) %>% mutate(sub = (A-B))
-#         data
-#     })
-#     output$table <- renderTable({
-#         dataframe()
-#     })
-#     output$plot <- renderPlot({
-#         if(!is.null(dataframe()))
-#             ggplot(dataframe(),aes(x=X,y=add))+geom_point()
-#     })
-#
-#
-#
-#
-#     # output$plotgraph <- renderPlot({
-#     #     raw_list <- reactive_data[1]
-#     #     raw_plots <- inspect_raw_data(raw_list)
-#     #     fig1 <- reactive({raw_plots$donor})
-#     #     fig2 <- reactive({raw_plots$acceptor})
-#     #     fig3 <- reactive({raw_plots$fret})
-#     #     fig_list <- list(fig1(),fig2(),fig3())
-#     #     to_delete <- !sapply(fig_list,is.null)
-#     #     fig_list <- fig_list[to_delete]
-#     #     if (length(ptlist)==0){
-#     #         return(NULL)
-#     #     }
-#     # grid.arrange(grobs=ptlist,ncol=length(ptlist))
-#
-# })
-#
+    file_index <- observeEvent(input$accept, label = "Accept", {
+        values$file_index <- values$file_index + 1
+        cat("accept ", values$file_index, "\n")
+        values$dataset_decisions[[length(values$dataset_decisions) + 1]] <- TRUE
+        View(values$dataset_decisions)
+    })
 
+    file_index <- observeEvent(input$remove, label = "Remove", {
+        values$file_index <- values$file_index + 1
+        cat("remove ", values$file_index, "\n")
+        values$dataset_decisions[[length(values$dataset_decisions) + 1]] <- FALSE
+        View(values$dataset_decisions)
+    })
+    })
+}
+
+    #output$
+
+    # output$plotgraph <- renderPlot({
+    #     raw_plots <- inspect_raw_data(new_data$file_list[1])
+    #     print("test")
+    #     fig1 <- raw_plots$donor
+    #     fig2 <- raw_plots$acceptor
+    #     fig3 <- raw_plots$fret
+    #     fig_list <- list(fig1,fig2,fig3)
+    #     to_delete <- !sapply(fig_list,is.null)
+    #     fig_list <- fig_list[to_delete]
+    #     if (length(fig_list)==0){
+    #         return(NULL)
+    #     }
+    #     print(fig1)
+        #grid.arrange(grobs=fig_list,ncol=length(fig_list))
+    #})
+    #print("test444")
+
+    # output$plotgraph <- renderPlot({
+    #     raw_list <- reactive_data[1]
+    #     raw_plots <- inspect_raw_data(raw_list)
+    #     fig1 <- reactive({raw_plots$donor})
+    #     fig2 <- reactive({raw_plots$acceptor})
+    #     fig3 <- reactive({raw_plots$fret})
+    #     fig_list <- list(fig1(),fig2(),fig3())
+    #     to_delete <- !sapply(fig_list,is.null)
+    #     fig_list <- fig_list[to_delete]
+    #     if (length(ptlist)==0){
+    #         return(NULL)
+    #     }
+    # grid.arrange(grobs=ptlist,ncol=length(ptlist))
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,63 +1,56 @@
 library(shiny)
 
 fluidPage(
-
     titlePanel("RFret"),
     sidebarLayout(
         sidebarPanel(
             tabsetPanel(
                 tabPanel("Input datasets",
-                    fileInput('datafile', 'Choose 1 or more FRET data files:',
-                        accept=c('text/csv',
-                            'text/comma-separated-values,text/plain',
-                            '.csv'),
-                            multiple = TRUE
-                            ),
-                    tags$hr(),
-                    textInput('skip_rows', 'How many rows should be skipped?', 4, width = 250),
-                    checkboxInput('header', 'Are the column names present?', TRUE),
-                    radioButtons('sep', 'Separator',
-                        c(Comma=',',
-                            Semicolon=';',
-                            Tab='\t'),
-                            ','),
-                    radioButtons('quote', 'Quote',
-                        c(None='',
-                            'Double Quote'='"',
-                            'Single Quote'="'"),
-                            '"'),
-                    checkboxInput('view_quality_filter',
-                        'Do you want to view the quality filter plots?', TRUE),
-                    downloadButton('downloadPlot', 'Download Plot')
-                    ),
+                         fileInput('data_file', 'Choose 1 or more FRET data files:',
+                                   accept=c('text/csv',
+                                            'text/comma-separated-values,text/plain',
+                                            '.csv'),
+                                   multiple = TRUE
+                         ),
+                         tags$hr(),
+                         textInput('skip_rows', 'How many rows should be skipped?', 4, width = 250),
+                         checkboxInput('header', 'Are the column names present?', TRUE),
+                         radioButtons('sep', 'Separator',
+                                      c(Comma=',',
+                                        Semicolon=';',
+                                        Tab='\t'),
+                                      ','),
+                         radioButtons('quote', 'Quote',
+                                      c(None='',
+                                        'Double Quote'='"',
+                                        'Single Quote'="'"),
+                                      '"'),
+                         checkboxInput('view_quality_filter',
+                                       'Do you want to view the quality filter plots?', TRUE),
+                         downloadButton('downloadPlot', 'Download Plot')
+                ),
                 tabPanel("Advanced Options",
-                    checkboxInput('option1', 'Option1', value = FALSE) ,
-                    checkboxInput('option2', 'Option2', value = FALSE) ,
-                    selectInput('option3', label ='Option3',
-                        choices=c("1" = 1,"2"=2,"3"=3),
-                        multiple=FALSE, selectize=TRUE,selected="1"),
-                    selectInput('option4', label ='Option4',
-                        choices=c("4" = 4,"5"=5,"6"=6),
-                        multiple=TRUE, selectize=TRUE,selected="4")
+                         checkboxInput('option1', 'Option1', value = FALSE) ,
+                         checkboxInput('option2', 'Option2', value = FALSE) ,
+                         selectInput('option3', label ='Option3',
+                                     choices=c("1" = 1,"2"=2,"3"=3),
+                                     multiple=FALSE, selectize=TRUE,selected="1"),
+                         selectInput('option4', label ='Option4',
+                                     choices=c("4" = 4,"5"=5,"6"=6),
+                                     multiple=TRUE, selectize=TRUE,selected="4")
                 ),
                 tabPanel("Help",
                          h5("Help information")
                 )
             )
         ),
-        # mainPanel("main panel",
-        #           column(6,plotOutput(outputId="plotgraph",
-        #                     width="500px",height="400px"))
-        # ),
-        mainPanel(
-            h3(
-                textOutput("List of files"),
-                #tableOutput('table'),
-                plotOutput('plot')
-                #textOutput(print(reactive_data$file_list)
-                #)
-            )
-        ),
-        position = "left"
+        mainPanel("",
+                  #h1(textOutput("value1")),
+                  h1(textOutput("filename")),
+                  plotOutput("raw_output"),
+                  actionButton("accept", "Accept"),
+                  actionButton("remove", "Remove"),
+                  actionButton("accept_all", "Accept All")
         )
+    )
 )
